592c3c000452965afa2012278a81325de8fb8bf9
inca <incarnate@whiteants.net>
2009-10-14 12:25:06 +0400

    Added transaction method to DAO.

--- circumflex-hibernate/src/main/scala/dao.scala
+++ circumflex-hibernate/src/main/scala/dao.scala
@@ -1,13 +1,37 @@
 package circumflex.hibernate
 
+
+import org.hibernate.HibernateException
+
 trait DAO[T] {
+
   def persistentClass: Class[T]
+
   def sessionContext: HibernateUtil
+
   def createCriteria = sessionContext.createCriteria(persistentClass)
+
   def findAll = createCriteria.list
+
   def refresh(entity: T) = sessionContext.currentSession.refresh(entity)
-  def save(entity: T) = sessionContext.currentSession.save(entity)
-  def saveOrUpdate(entity: T) = sessionContext.currentSession.saveOrUpdate(entity)
-  def update(entity: T) = sessionContext.currentSession.update(entity)
-  def delete(entity: T) = sessionContext.currentSession.delete(entity)
+
+  def transaction(actions: HibernateSession => Unit)(error: HibernateException => Unit) = {
+    val session = sessionContext.openSession
+    session.begin
+    try {
+      actions(session)
+      session.commit
+    } catch {
+      case e: HibernateException => {
+        session.rollback
+        error(e)
+      }
+      case e => {
+        session.rollback
+        throw e
+      }
+    } finally {
+      if (session.isOpen) session.close
+    }
+  }
 }
