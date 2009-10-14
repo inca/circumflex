eb6faa93d882084b0c1af5f8e2fb191b61b8c4c9
inca <incarnate@whiteants.net>
2009-10-14 16:12:15 +0400

    Updated validate() helper.

--- circumflex-hibernate/src/main/scala/helpers.scala
+++ circumflex-hibernate/src/main/scala/helpers.scala
@@ -1,6 +1,7 @@
 package circumflex.hibernate
 
 import circumflex.core.HttpResponse
+import javax.validation.ConstraintViolation
 
 
 trait HibernateHelper extends TransactionContext {
@@ -8,6 +9,10 @@ trait HibernateHelper extends TransactionContext {
   def tx(actions: HibernateSession => HttpResponse)
         (error: Throwable => HttpResponse) = transaction(actions)(error)
 
-  def validate[T](obj: T) = provider.validator.validate(obj)
+  def validate[T](obj: T): Option[Seq[ConstraintViolation[T]]] = {
+    val set = provider.validator.validate(obj)
+    if (set.size == 0) None
+    else Some(set.toArray.map(_.asInstanceOf[ConstraintViolation[T]]))
+  }
 
 }
