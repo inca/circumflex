e58cd833ec1b8369be49ef081ef3bc7100edc702
inca <incarnate@whiteants.net>
2009-10-27 16:19:05 +0300

    Working on ORM.

--- circumflex-orm/src/main/scala/meta.scala
+++ circumflex-orm/src/main/scala/meta.scala
@@ -1,56 +1,29 @@
 package ru.circumflex.orm
 
-class ORMException(msg: String) extends Exception(msg)
-
 /**
- * Provides base functionality for all domain model objects.
+ * Provides base functionality for generating domain model schema,
+ * as well as validating, quering, inserting, deleting and updating
+ * domain model objects (a.k.a. <code>records</code>).
+ * In general there should be only one table instance per record class
+ * (a singleton object, or, more conveniantly, the companion object).
  */
-trait Record {
-
+abstract class Table(val schema: String,
+                     val name: String) {
   /**
-   * Configuration object used for all persistence-related stuff.
-   * Override it if you want to provide your own configuration.
+   * Configuration object is used for all persistence-related stuff.
+   * Override it if you want to use your own configuration implementation.
    * @return DefaultConfiguration by default
    */
   def configuration: Configuration = DefaultConfiguration
 
-  def validate(): Unit = {}
-  def save(): Unit = {}
-  def saveOrUpdate(): Unit = {}
-  def update(): Unit = {}
-  def delete(): Unit = {}
-
 }
 
-trait MetaRecord extends Record {
-  override def validate() = throw new IllegalAccessException("Method validate() cannot be called on metarecord.")
-  def saveOrUpdate() = throw new IllegalAccessException("Method saveOrUpdate() cannot be called on metarecord.")
-  def save() = throw new IllegalAccessException("Method save() cannot be called on metarecord.")
-  def update() = throw new IllegalAccessException("Method update() cannot be called on metarecord.")
-  def delete() = throw new IllegalAccessException("Method delete() cannot be called on metarecord.")
+abstract class Column(val table: Table,
+                      val name: String,
+                      val sqlType: String,
+                      val nullable: Boolean,
+                      val misc: String) {
+  
 }
 
 
-/**
- * Provides base functionality for persistent fields.
- */
-abstract class Field[T](owner: Record) {
-
-  private var value: Option[T] = None
-
-  def apply(): T = this.value.get
-
-  def update(value: T): Unit = {
-    this.value = value
-  }
-
-}
-
-
-class Person extends Record {
-
-}
-
-object Person extends Person with MetaRecord {
-
-}
