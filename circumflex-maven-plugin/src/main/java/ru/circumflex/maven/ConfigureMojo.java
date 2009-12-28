c67412c2ea9f7132dd2c0a09590c093f8e6c8d4d
incarnate <incarnate@whiteants.net>
2009-12-29 00:49:03 +0300

    Improved (and greatly simplified) ConfigureMojo.

--- circumflex-maven-plugin/src/main/java/ru/circumflex/maven/ConfigureMojo.java
+++ circumflex-maven-plugin/src/main/java/ru/circumflex/maven/ConfigureMojo.java
@@ -28,6 +28,7 @@ package ru.circumflex.maven;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -43,11 +44,6 @@ import java.util.Properties;
 public class ConfigureMojo extends AbstractMojo {
 
     /**
-     * @parameter
-     */
-    private Properties properties;
-
-    /**
      * @parameter default-value="${project.build.outputDirectory}/cx.properties"
      */
     private File targetFile;
@@ -57,13 +53,19 @@ public class ConfigureMojo extends AbstractMojo {
      */
     private boolean skipUnresolved;
 
+    /**
+     * @parameter expression="${project}"
+     * @readonly
+     */
+    private MavenProject project;
+
     public void execute() throws MojoExecutionException {
         try {
             PrintWriter fw = new PrintWriter(targetFile);
             getLog().info("Writing Circumflex configuration to " + targetFile);
             try {
-                for (Object key : properties.keySet()) {
-                    String value = properties.get(key).toString();
+                for (Object key : project.getProperties().keySet()) {
+                    String value = project.getProperties().get(key).toString().trim();
                     if (!(skipUnresolved && value.startsWith("${") && value.endsWith("}")))
                         fw.println(key + "=" + value);
                     else getLog().warn("Property with key " + key + " is unresolved. To include it, set 'skipUnresolved' to false.");
