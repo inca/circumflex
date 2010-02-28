d6908945e27b67ffe68de083ba006a1362eca22e
metheora <metheora@metheora-book.(none)>
2010-02-28 13:32:35 +0300

    Added oneToOne helper.

--- circumflex-orm/src/main/scala/record.scala
+++ circumflex-orm/src/main/scala/record.scala
@@ -47,25 +47,19 @@ import ORM._
  */
 abstract class Record[R] {
   val internalUUID = UUID.randomUUID.toString
-
   val fieldsMap = new HashMap[Column[_, R], Any]()
-
   val relation: Relation[R] = ORMRegistry.getRelation(this)
 
   def primaryKey: Option[_] = fieldsMap.get(relation.primaryKey.column)
-
   def identified_?() = primaryKey != None
 
   /* FIELDS-RELATED STUFF */
 
   def field[T](col: Column[T, R]) = new ColumnField(this, col)
-
   def getField[T](col: Column[T, R]): Option[T] =
     fieldsMap.get(col).asInstanceOf[Option[T]]
-
   def setField[T](col: Column[T, R], value: T): Unit =
     setField(col, Some(value))
-
   def setField[T](col: Column[T, R], value: Option[T]) =
     value match {
       case Some(value) => fieldsMap += (col -> value)
@@ -76,28 +70,21 @@ abstract class Record[R] {
 
   def manyToOne[P](association: Association[R, P]) =
     new ManyToOne[R, P](this, association)
-
   def oneToMany[C](association: Association[C, R]) =
     new OneToMany[C, R](this, association)
+  def oneToOne[C](association: Association[C, R]) =
+    new OneToOne[C, R](this, association)
 
   /* PERSISTENCE-RELATED STUFF */
 
   def validate(): Option[Seq[ValidationError]] = relation.validate(this)
-
   def validate_!(): Unit = relation.validate_!(this)
-
   def insert(): Int = relation.insert(this)
-
   def insert_!(): Int = relation.insert_!(this)
-
   def update(): Int = relation.update(this)
-
   def update_!(): Int = relation.update_!(this)
-
   def save(): Int = relation.save(this)
-
   def save_!(): Int = relation.save_!(this)
-
   def delete(): Int = relation.delete(this)
 
   /* EQUALITY AND OTHER STUFF */
@@ -107,50 +94,39 @@ abstract class Record[R] {
       this.primaryKey.getOrElse(this.internalUUID) == r.primaryKey.getOrElse(r.internalUUID)
     case _ => false
   }
-
   override def hashCode = this.primaryKey.getOrElse(internalUUID).hashCode
-
   override def toString = relation.relationName + ": " + this.fieldsMap.toString
 }
 
 trait Field[T] {
-
   def default(value: T): this.type = {
     set(value)
     return this
   }
-
   def get: Option[T]
-
   def getOrElse(defaultValue: T): T = get match {
     case Some(value) => value
     case _ => defaultValue
   }
-
   def set(value: T): Unit
   def setNull: Unit
   def <=(value: T): Unit = set(value)
   def :=(value: T): Unit = set(value)
-
   override def toString = get match {
     case Some(value) => value.toString
     case None => ""
   }
-
 }
 
 trait Collection[T] {
-
   def default(values: T*): this.type = {
     set(values.toList)
     return this
   }
-
   def get: Seq[T]
   def set(value: Seq[T]): Unit
   def <=(value: Seq[T]): Unit = set(value)
   def :=(value: Seq[T]): Unit = set(value)
-
   override def toString = get.toString
 }
 
@@ -158,7 +134,6 @@ trait Collection[T] {
 class ColumnField[T, R](val record: Record[R],
                         val column: Column[T, R])
         extends Field[T] {
-
   def get: Option[T] = record.getField(column)
   def set(value: T): Unit = record.setField(column, value)
   def setNull: Unit = record.setField(column, None)
@@ -170,17 +145,16 @@ class ManyToOne[C, P](val record: Record[C],
 
   protected val r = record.asInstanceOf[C]
 
-  def get: Option[P] =
-    tx.getCachedMTO(association, r) match {
-      case None => record.getField(association.childColumn) match {     // lazy-fetch a parent
-        case Some(localVal) => association.fetchManyToOne(localVal) match {
-          case Some(mto : P) =>
-            tx.updateMTOCache(association, r, mto)
-            Some(mto)
-          case _ => None
-        } case _ => None
-      } case v => v
-    }
+  def get: Option[P] = tx.getCachedMTO(association, r) match {
+    case None => record.getField(association.childColumn) match {     // lazy-fetch a parent
+      case Some(localVal) => association.fetchManyToOne(localVal) match {
+        case Some(mto : P) =>
+          tx.updateMTOCache(association, r, mto)
+          Some(mto)
+        case _ => None
+      } case _ => None
+    } case v => v
+  }
 
   protected def setManyToOne[P](value: Option[P]): Unit = value match {
     case Some(value: P) =>
@@ -193,6 +167,36 @@ class ManyToOne[C, P](val record: Record[C],
 
   def set(value: P): Unit = setManyToOne(Some(value))
   def setNull: Unit = setManyToOne(None)
+
+}
+
+class OneToOne[C, P](val record: Record[P],
+                     val association: Association[C, P])
+        extends Field[C] {
+
+  protected val r = record.asInstanceOf[P]
+
+  def get: Option[C] = tx.getCachedOTM(association, r) match {
+    case Some(seq: Seq[C]) if (seq.size > 0) => Some(seq(0))
+    case None => record.primaryKey match {
+      case Some(refVal) =>            // lazy-fetch
+        val children = association.fetchOneToMany(refVal)
+        tx.updateOTMCache(association, r, children)
+        return this.get
+      case _ => None
+    }
+    case _ => None
+  }
+
+  protected def setOneToOne[C](value: Option[C]): Unit = value match {
+    case Some(value: C) =>
+      tx.updateOTMCache(association.asInstanceOf[Association[Any,P]], r, List(value))
+    case None =>
+      tx.updateOTMCache(association.asInstanceOf[Association[Any,P]], r, Nil)
+  }
+
+  def set(value: C): Unit = setOneToOne(Some(value))
+  def setNull: Unit = setOneToOne(None)
 
 }
 
@@ -213,9 +217,8 @@ class OneToMany[C, P](val record: Record[P],
     }
   }
 
-  protected def setOneToMany[C](value: Seq[C]): Unit = {
+  protected def setOneToMany[C](value: Seq[C]): Unit =
     tx.updateOTMCache(association.asInstanceOf[Association[Any,P]], r, value)
-  }
 
   def set(value: Seq[C]): Unit = setOneToMany(value)
 
