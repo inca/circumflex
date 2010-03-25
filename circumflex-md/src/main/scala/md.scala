259774425d12f285d5e51a3dd239d4154bc5abc0
incarnate <incarnate@whiteants.net>
2010-03-25 22:36:40 +0300

    So far I've been through the lyrics. No actual coding.

--- circumflex-md/src/main/scala/md.scala
+++ circumflex-md/src/main/scala/md.scala
@@ -6,50 +6,71 @@ import java.util.regex._
 
 /**
  * This utility converts a plain text written in [Markdown][1] into HTML fragment.
- * The `apply(String)` method does all the work.
+ * The typical usage is:
+ *
+ *     val md = Markdown(myMarkdownText)
+ *     // do some stuff
  *
  *  [1]: http://daringfireball.net/projects/markdown/syntax "Markdown Syntax"
  */
 object Markdown {
 
-  /* # Regex patterns */
+  /* ## Regex patterns */
 
   // standardize line endings
-  private val rLineEnds = Pattern.compile("\\r\\n|\\r")
+  val rLineEnds = Pattern.compile("\\r\\n|\\r")
   // strip out whitespaces in blank lines
-  private val rBlankLines = Pattern.compile("^ +$", Pattern.MULTILINE)
+  val rBlankLines = Pattern.compile("^ +$", Pattern.MULTILINE)
   // replace tabs with spaces
-  private val rTabs = Pattern.compile("\\t")
+  val rTabs = Pattern.compile("\\t")
 
-  /* # The Processing Stuff */
+  /* ## Commons */
 
-  // Normalization includes following stuff:
-  //
-  // * replace DOS- and Mac-specific line endings with `\n`;
-  // * replace tabs with spaces;
-  // * reduce all blank lines (i.e. lines containing only spaces) to empty strings.
+
+
+  /* ## The `apply` method */
 
   /**
    * Converts the `source` from Markdown to HTML.
    */
-  def apply(source: String): String = {
-    val text = new StringEx(source)
-        .replaceAll(rLineEnds, "\n")
-        .replaceAll(rTabs, "    ")
-        .replaceAll(rBlankLines, "")
-
-    return text.toString
-  }
+  def apply(source: String): String = new MarkdownText(source).toHtml
 
 }
 
-/* # Utility stuff */
+/* # The Processing Stuff */
 
 /**
- * Wraps the source into mutable StringBuilder with utility methods.
+ * We collect all processing logic for the input characters within this class.
+ * It is not supposed to play with the stuff inside, but just in case you decide to,
+ * there's a couple of things you should know.
  */
-class StringEx(source: CharSequence) {
-  private var text = new StringBuffer(source)
+class MarkdownText(source: CharSequence) {
+  /**
+   * The first one: `source` gets wrapped into `StringBuffer`. See, while in general
+   * `StringBuilder` provides more speed, for some reason Java regular expressions
+   * provide support only for buffers (that are organized in a thread-safe manner, which
+   * tend to be much slower than builder). Since we use a single-thread model here, we
+   * find that fact offensive to all the `StringBuilder` thang, but sincerely can't do
+   * no damn thing about it.
+   */
+  protected var text = new StringBuffer(source)
+
+  /**
+   * The second one: we use the default `Markdown` singleton to lookup precompiled regexes.
+   * I know it sounds like saving on buying bread, but we are really concerned about
+   * the performance of that little thingy =)
+   */
+  import Markdown._
+
+  /**
+   * And the last one: as I mentioned before, we use a single-thread model here. The `toHtml`
+   * method is supposed to be called only once per instance, and, since the `text` is mutable
+   * calling it twice may cause *very* unpredictible results.
+   *
+   * Have fun! 
+   */
+
+  /* ## Utility methods */
 
   /**
    * A convenient equivalent to `String.replaceAll` that accepts `Pattern`,
@@ -82,9 +103,21 @@ class StringEx(source: CharSequence) {
     return this
   }
 
-  /**
-   * Emits the wrapped content.
-   */
-  override def toString = text.toString
-}
+  /* ## Processing methods */
 
+  /**
+   * Normalization includes following stuff:
+   *
+   * * replace DOS- and Mac-specific line endings with `\n`;
+   * * replace tabs with spaces;
+   * * reduce all blank lines (i.e. lines containing only spaces) to empty strings.
+   */
+  def normalize(): this.type =replaceAll(rLineEnds, "\n")
+        .replaceAll(rTabs, "    ")
+        .replaceAll(rBlankLines, "")
+
+  def toHtml(): String = {
+
+  }
+
+}
