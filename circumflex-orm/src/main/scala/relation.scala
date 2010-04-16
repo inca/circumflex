1a58c6ea9c962d6909b0161ee2679a1e0b9d088c
inca <incarnate@whiteants.net>
2010-04-16 17:42:51 +0400

    ColumnMeta sketch

---
+++ circumflex-orm/src/main/scala/relation.scala
@@ -0,0 +1,18 @@
+package ru.circumflex.orm
+
+import org.slf4j.LoggerFactory
+import ORM._
+
+/* ## Relation */
+
+/**
+ * *Relation* is a cornerstone of relational model. In general, each instance of
+ * pesistent class is stored as a single record in a relation corresponding to that
+ * class. Since Circumflex ORM employs the Active Relation design approach to
+ * persistence, each persistent class should subclass `Relation`.
+ */
+class Relation {
+
+  
+
+}
