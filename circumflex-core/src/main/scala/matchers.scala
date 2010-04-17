81b1155a125fd9c30a3b6a409a79ee35e038f140
Mauro Rocchi <maurocchi@gmail.com>
2010-04-17 14:58:18 +0200

    Implemented UriMather (to match like Sinatra) and simplified matcher.scala

--- circumflex-core/src/main/scala/matchers.scala
+++ circumflex-core/src/main/scala/matchers.scala
@@ -3,30 +3,40 @@ package ru.circumflex.core
 import java.net.URLDecoder
 import javax.servlet.http.HttpServletRequest
 import util.matching.Regex
+import collection.mutable.ListBuffer
+import RichRegex._
 
 trait RequestMatcher {
 
-  def extractMatches(regex: Regex,
-                     src: String,
-                     groupPrefix: String): Option[Map[String, String]] = {
-    val m = regex.pattern.matcher(src)
-    if (m.matches) {
-      var matches = Map[String, String]()
-      (1 to m.groupCount).foreach(ix => {
-        matches += groupPrefix + ix -> m.group(ix)
-      })
-      Some(matches)
-    } else None
+  def apply(request: HttpServletRequest): Option[Map[String, String]]
+
+}
+
+case class UriMatcher(val path: String) extends RequestMatcher {
+
+  val keys = new ListBuffer[String]()
+  val uriRegex = """:(\w+)|[\*.+()]""".r.replaceAllInF(path) {
+    case "*" =>
+      keys += "uri$" + (keys.length + 1)
+      "(.*?)"
+
+    case sym@("." | "+" | "(" | ")") =>
+      "\\" + sym
+
+    case name =>
+      keys += name.substring(1)
+      "([^/?&#]+)"
   }
 
-  def apply(request: HttpServletRequest): Option[Map[String, String]]
+  def apply(request: HttpServletRequest) =
+    uriRegex.r.allMatches(URLDecoder.decode(request.getRequestURI, "UTF-8"), i => keys(i - 1))
 
 }
 
 case class UriRegexMatcher(val uriRegex: String) extends RequestMatcher {
 
   def apply(request: HttpServletRequest) =
-    extractMatches(uriRegex.r, URLDecoder.decode(request.getRequestURI, "UTF-8"), "uri$")
+    uriRegex.r.allMatches(URLDecoder.decode(request.getRequestURI, "UTF-8"), "uri$" + _)
 
 }
 
@@ -34,16 +44,16 @@ case class HeadersRegexMatcher(val criteria: (String, String)*) extends RequestM
 
   def apply(request: HttpServletRequest): Option[Map[String, String]] = {
     var params = Map[String,String]()
-    criteria.toList.foreach(
-      crit => matchHeader(crit._1, request.getHeader(crit._1), crit._2.r) match {
+     for ((name, pattern) <- criteria.toList)
+      matchHeader(name, request.getHeader(name), pattern.r) match {
         case Some(p) => params ++= p
-        case _ => return None
-      })
+        case None    => return None
+      }
     Some(params)
   }
 
   def matchHeader(headerName: String, headerValue: String, crit: Regex): Option[Map[String, String]] =
     if (headerValue == null) None
-    else extractMatches(crit, headerValue, headerName + "$")
+    else crit.allMatches(headerValue, headerName + "$" + _)
 
 }
