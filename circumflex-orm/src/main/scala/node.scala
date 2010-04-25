29169be4a02000ec0cf98be4d98cb145f0f372ce
inca <incarnate@whiteants.net>
2010-04-25 18:02:38 +0400

    Removed inheritance from relations in nodes.

--- circumflex-orm/src/main/scala/node.scala
+++ circumflex-orm/src/main/scala/node.scala
@@ -6,10 +6,12 @@ import ORM._
 
 /**
  * **Relation Node** is essentially a wrapper around `Relation` that
- * provides an `alias` so that they can be used in SQL queries.
+ * provides an `alias` so that it can be used in SQL queries.
  */
 abstract class RelationNode[R <: Record[R]](val relation: Relation[R])
-        extends Relation[R] with SQLable with Cloneable {
+        extends SQLable with Cloneable {
+
+  // ### Commons
 
   protected[orm] var _alias = "this"
 
