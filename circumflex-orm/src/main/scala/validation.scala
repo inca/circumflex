188ec200b0b3b41839ca642a5d114f6c5f7c742e
inca <incarnate@whiteants.net>
2010-06-11 16:24:12 +0400

    Added unique validator.

--- circumflex-orm/src/main/scala/validation.scala
+++ circumflex-orm/src/main/scala/validation.scala
@@ -69,29 +69,36 @@ class RecordValidator[R <: Record[R]] {
     _validators ++= List(validator)
     return this
   }
-  def notNull(f: R => Field[_]): this.type =
-    add(r => {
-      val field = f(r)
-      if (field.null_?) Some(new ValidationError(field.uuid, "null")) else None
-    })
-  def notEmpty(f: R => TextField): this.type =
-    add(r => {
-      val field = f(r)
-      if (field.null_?)
-        Some(new ValidationError(field.uuid, "null"))
-      else if (field.getValue.trim == "")
-        Some(new ValidationError(field.uuid, "empty"))
-      else None
-    })
-  def pattern(f: R => TextField, regex: String, key: String = "pattern"): this.type =
-    add(r => {
-      val field = f(r)
-      if (field.null_?)
-        None
-      else if (!field.getValue.matches(regex))
-        Some(new ValidationError(field.uuid, key, "regex" -> regex, "value" -> field.getValue))
-      else None
-    })
+  def notNull(f: R => Field[_]): this.type = add(r => {
+    val field = f(r)
+    if (field.null_?) Some(new ValidationError(field.uuid, "null")) else None
+  })
+  def notEmpty(f: R => TextField): this.type = add(r => {
+    val field = f(r)
+    if (field.null_?)
+      Some(new ValidationError(field.uuid, "null"))
+    else if (field.getValue.trim == "")
+      Some(new ValidationError(field.uuid, "empty"))
+    else None
+  })
+  def pattern(f: R => TextField, regex: String, key: String = "pattern"): this.type = add(r => {
+    val field = f(r)
+    if (field.null_?)
+      None
+    else if (!field.getValue.matches(regex))
+      Some(new ValidationError(field.uuid, key, "regex" -> regex, "value" -> field.getValue))
+    else None
+  })
+  def unique(f: R => Field[_], key: String = "unique"): this.type = add(r => {
+    val field = f(r)
+    r.relation.criteria.add(field EQ field()).unique.map(a => new ValidationError(field.uuid, key))
+  })
+  def uniqueAll(f: R => Seq[Field[_]], key: String = "unique"): this.type = add(r => {
+    val fields = f(r)
+    val crit = r.relation.criteria
+    fields.foreach(f => crit.add(f EQ f()))
+    crit.unique.map(a => new ValidationError(r.uuid, key))
+  })
 }
 
 
