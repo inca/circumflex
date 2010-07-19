71775a4ee3608f9fcf21a23aa743a3d0947e21c0
inca <incarnate@whiteants.net>
2010-07-19 18:13:58 +0400

    Remake on FTL template loading theme.

--- circumflex-core/src/main/scala/response.scala
+++ circumflex-core/src/main/scala/response.scala
@@ -2,8 +2,8 @@ package ru.circumflex.core
 
 import javax.servlet.http.HttpServletResponse
 import org.apache.commons.io.IOUtils
-import java.io.{FileInputStream, File, OutputStream}
 import javax.activation.MimetypesFileTypeMap
+import java.io.{Writer, FileInputStream, File, OutputStream}
 
 /**
  * Represents an `HttpServletResponse` wrapper for committing responses.
@@ -16,19 +16,17 @@ trait HttpResponse {
    * specified `HttpServletResponse` and flushes any associated output.
    * If specified response is committed, returns silently.
    */
-  def apply(response: HttpServletResponse) {
-    if (!response.isCommitted) {
-      response.setCharacterEncoding("UTF-8")
-      response.setContentType(ctx.contentType.getOrElse("text/html"))
-      response.setStatus(ctx.statusCode)
-    }
+  def apply(response: HttpServletResponse) = if (!response.isCommitted) {
+    response.setCharacterEncoding("UTF-8")
+    response.setContentType(ctx.contentType.getOrElse("text/html"))
+    response.setStatus(ctx.statusCode)
   }
 }
 
 object EmptyResponse extends HttpResponse
 
 case class ErrorResponse(val errorCode: Int, val msg: String) extends HttpResponse {
-  override def apply(response: HttpServletResponse) {
+  override def apply(response: HttpServletResponse) = {
     ctx.statusCode = errorCode
     response.sendError(errorCode, msg)
   }
@@ -39,21 +37,21 @@ case class RedirectResponse(val url: String) extends HttpResponse {
 }
 
 case class TextResponse(val text: String) extends HttpResponse {
-  override def apply(response: HttpServletResponse) {
+  override def apply(response: HttpServletResponse) = {
     super.apply(response)
     response.getWriter.print(text)
   }
 }
 
 case class BinaryResponse(val data: Array[Byte]) extends HttpResponse {
-  override def apply(response: HttpServletResponse) {
+  override def apply(response: HttpServletResponse) = {
     super.apply(response)
     response.getOutputStream.write(data)
   }
 }
 
 case class FileResponse(val file: File) extends HttpResponse {
-  override def apply(response: HttpServletResponse) {
+  override def apply(response: HttpServletResponse) = {
     // determine mime type by extension
     if (ctx.contentType.isEmpty)
       ctx.contentType = (new MimetypesFileTypeMap).getContentType(file)
@@ -70,18 +68,15 @@ case class FileResponse(val file: File) extends HttpResponse {
 
 case class DirectStreamResponse(val streamingFunc: OutputStream => Unit)
     extends HttpResponse {
-  override def apply(response: HttpServletResponse) {
+  override def apply(response: HttpServletResponse) = {
     super.apply(response)
     streamingFunc(response.getOutputStream)
   }
 }
 
-case class ContextualResponse(f: CircumflexContext => HttpResponse) extends HttpResponse {
-  override def apply(response: HttpServletResponse) =
-    try {
-      f(ctx)(response)
-    }
-    catch {
-      case e: MatchError => ErrorResponse(400, "" + e)(response) // TODO: log error
-    }
+case class WriterResponse(val f: Writer => Unit) extends HttpResponse {
+  override def apply(response: HttpServletResponse) = {
+    super.apply(response)
+    f(response.getWriter)
+  }
 }
---
+++ circumflex-ftl/src/main/scala/config.scala
@@ -0,0 +1,78 @@
+package ru.circumflex.freemarker
+
+import _root_.freemarker.cache._
+import _root_.ru.circumflex.md.Markdown
+import ru.circumflex.core._
+import freemarker.template._
+import freemarker.core.Environment
+import java.io.StringWriter
+
+class FTL extends Configuration {
+
+  // ## Loaders
+  protected var _loaders: Seq[TemplateLoader] = Nil
+  def loaders = _loaders
+  def addLoader(loader: TemplateLoader): this.type = try {
+    _loaders ++= List(loader)
+    setTemplateLoader(new MultiTemplateLoader(loaders.toArray))
+    return this
+  } catch {
+    case e =>
+      cxLog.error("Could not add loader " + loader + " to FreeMarker configuration.")
+      return this
+  }
+  def setLoaders(ldrs: TemplateLoader*): this.type = {
+    _loaders = Nil
+    ldrs.foreach(l => addLoader(l))
+    return this
+  }
+
+  // ## Defaults
+
+  setObjectWrapper(new ScalaObjectWrapper())
+  setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER)
+  setDefaultEncoding("utf-8")
+  setSharedVariable("md", MarkdownDirective)
+
+  // ## Rendering methods
+
+  def ftl(template: String): Nothing =
+    throw new RouteMatchedException(new WriterResponse(w => getTemplate(template).process(ctx, w)))
+  def ftl(template: String, statusCode: Int): Nothing = {
+    ctx.statusCode = statusCode
+    ftl(template)
+  }
+  def ftl(template: String, params: Pair[String, Any]*): String =
+    ftl(template, Map[String, Any](params: _*))
+  def ftl(template: String, root: Any): String = {
+    val result = new StringWriter
+    getTemplate(template).process(root, result)
+    return result.toString
+  }
+}
+
+/**
+ * This FreeMarker configuration implies following:
+ * <ul>
+ * <li>templates are loaded from ${webapp-Context}/templates directory or,
+ * if failed, from webapp classpath;</li>
+ * <li>all template errors result in exception to be thrown to controller;</li>
+ * <li>character encoding defaults to UTF-8;</li>
+ * <li>use Circumflex default object wrapper for Scala core types.</li>
+ * </ul>
+ */
+object FTL extends FTL {
+  addLoader(new WebappTemplateLoader(ctx.request.getSession.getServletContext, "/templates"))
+  addLoader(new ClassTemplateLoader(getClass, "/"))
+}
+
+object MarkdownDirective extends TemplateDirectiveModel {
+  def execute(env: Environment,
+              params: java.util.Map[_, _],
+              loopVars: Array[TemplateModel],
+              body: TemplateDirectiveBody) = {
+    val nested = new StringWriter
+    body.render(nested)
+    env.getOut.write(Markdown(nested.toString))
+  }
+}
