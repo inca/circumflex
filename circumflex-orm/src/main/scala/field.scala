93c27a0ec578ced9bc5b7fefdaa1180175918742
inca <incarnate@whiteants.net>
2010-10-15 16:26:13 +0400

    Now handling null values in XML as well.

--- circumflex-orm/src/main/scala/field.scala
+++ circumflex-orm/src/main/scala/field.scala
@@ -1,5 +1,6 @@
 package ru.circumflex.orm
 
+import ru.circumflex.core._
 import java.util.Date
 import xml._
 import java.sql.ResultSet
@@ -100,5 +101,5 @@ class XmlField[R <: Record[_, R]](name: String, record: R)
     extends XmlSerializable[Elem, R](name, record, dialect.xmlType) {
   def from(str: String): Option[Elem] = Some(XML.loadString(str))
   override def read(rs: ResultSet, alias: String) =
-    from(rs.getString(alias))
+    any2option(rs.getString(alias)).flatMap(x => from(x))
 }
