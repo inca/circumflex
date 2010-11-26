8b8c5b77f6392e40246b08081d2a3bb8396e2bc2
inca <incarnate@whiteants.net>
2010-11-26 18:21:22 +0300

    Fixed url encoding in redirects.

--- circumflex-web/src/main/scala/package.scala
+++ circumflex-web/src/main/scala/package.scala
@@ -8,6 +8,7 @@ import javax.activation.MimetypesFileTypeMap
 import java.io._
 import org.apache.commons.io.IOUtils
 import collection.mutable.Map
+import java.net.URLEncoder._
 
 /*!# The `web` Package
 
@@ -171,7 +172,7 @@ package object web {
     response.body(r => r.sendError(statusCode, message)).flush_!
   def sendRedirect(url: String, flashes: (String, Any)*): Nothing = {
     flashes.foreach(kv => flash(kv._1) = kv._2)
-    response.body(r => r.sendRedirect(url)).flush_!
+    response.body(r => r.sendRedirect(encode(url, "UTF-8"))).flush_!
   }
   def sendFile(file: File, filename: String = ""): Nothing = {
     // if filename is provided, add `Content-Disposition` header
