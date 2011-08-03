e4b410d0d0f3ae164d799f9dc684f7720dd9892b
inca <incarnate@whiteants.net>
2011-08-03 13:09:18 +0400

    Default configuration via cx.properties is not mandatory anymore (thanks to Yang)

--- circumflex-orm/src/main/scala/package.scala
+++ circumflex-orm/src/main/scala/package.scala
@@ -18,7 +18,8 @@ package object orm {
 
   val ORM_LOG = new Logger("ru.circumflex.orm")
 
-  val DEFAULT_ORM_CONF = new DefaultORMConfiguration("")
+  lazy val DEFAULT_ORM_CONF = cx.instantiate[ORMConfiguration](
+    "orm.configuration", new DefaultORMConfiguration(""))
   def ormConf = ctx.get("orm.conf") match {
     case Some(c: ORMConfiguration) => c
     case _ => DEFAULT_ORM_CONF
