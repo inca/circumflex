b4e9491c0528fda083f616f75033f51a13b33ba6
inca <incarnate@whiteants.net>
2012-12-15 01:23:46 +0400

    Finished docs for circumflex.scala and context.scala so far.

--- core/src/main/scala/circumflex.scala
+++ core/src/main/scala/circumflex.scala
@@ -6,24 +6,33 @@ import java.util.{MissingResourceException, ResourceBundle, Locale}
 
 /*!# Configuration API
 
-This singleton can be used to retrieve Circumflex configuration parameters
-throughout your application.
+The `Circumflex` singleton can be used to set and retrieve
+Circumflex configuration parameters in your application.
 
-Configuration parameters are read from `cx.properties` which should be available
-in classpath.
+Configuration parameters are read from `cx.properties`
+which should be available in classpath. The location of `cx.properties`
+can be overridden with the `cx.configuration` System property.
 
-You can also configure your application in runtime, just add your configuration
-parameters into `Circumflex` using methods of Scala's `Map` interface.
-Note, however, that `Circumflex` singleton is not thread-safe, so it is a best
-practice to only set configuration parameters inside some initialization block
-which will be executed only once by only one thread.
+This singleton is based on `KeyValueCoercion`
+(see [[/core/src/main/scala/kvc.scala]]).
 
-[Circumflex Maven Plugin](http://circumflex.ru/projects/maven-plugin/index.html)
-enables you to configure your application at build time using Maven properties
-(specified either in application's `pom.xml` or in `settings.xml`) and system
-properties (specified in command line). This is very robust production scenario,
-because it allows different configurations in different environments without
-manual filtering sources and resources.
+## Runtime configuration
+
+You can also configure your application in runtime, just add your
+configuration parameters into `Circumflex` just like you do it
+with regular mutable Scala `Map`.
+
+Note, however, that `Circumflex` singleton is not thread-safe,
+so it is a best practice to only set configuration parameters
+inside some initialization block which will be executed only
+once by single thread.
+
+Circumflex Maven Plugin enables you to configure your application
+at build time using Maven properties (specified either in
+application `pom.xml` or in `settings.xml`) and system properties
+(specified in command line). This is very robust production scenario,
+because it allows different configurations in different environments
+without having to fall back to manual sources and resources filtering.
 */
 object Circumflex extends HashMap[String, Any] with KeyValueCoercion {
 
--- core/src/main/scala/context.scala
+++ core/src/main/scala/context.scala
@@ -5,42 +5,45 @@ import collection.mutable.{ListBuffer, HashMap}
 
 /*!# Context API
 
-Context is a thread-local container which allows you to share objects (also known as
-context variables) within one logical scope.
+Context is a thread-local container which allows you to share
+objects (also known as context variables) within one logical scope
+bound to the executing thread. Such logical scope could be anything:
+ database transaction, HTTP request, user session within GUI form, etc.
 
-Such logical scope could be anything: database transaction, HTTP request, user
-session within GUI form, etc. Within this scope you can obtain current context by
-calling `Context.get` method (or using `ctx` method of package `pro.savant.circumflex.core`).
+Within this scope you can obtain current context by calling
+`Context.get` method (or using `ctx` method of package
+`pro.savant.circumflex.core`).
 
-Most Circumflex components depend on context and, therefore, can only be run
-inside context-aware code. Application is responsible for maintaining context
-lifecycle. For example, [Circumflex Web Framework](http://circumflex.ru/projects/web/index.html)
-takes care of context initialization and finalization inside `CircumflexFilter`.
-See [Context Lifecycle](#lifecycle) for more information.
+Most Circumflex components depend on context and, therefore,
+can only be run inside context-aware code. Your application is
+responsible for maintaining context lifecycle.
 
-Circumflex context also extends `KeyValueCoercion` so you may access instantiation
-fancies as well as coercing parameters to expected types.
+For example, Circumflex Web Framework takes care of context
+initialization and finalization inside `CircumflexFilter` (see [[web/src/main/scala/filter.scala]]).
 
-# Context Lifecycle {#lifecycle}
+Circumflex context is based on `KeyValueCoercion`
+(see [[/core/src/main/scala/kvc.scala]]).
 
-In order to maintain context scope an application should properly initialize
-and destroy contexts. It is done by using `Context.init` and `Context.destroy`
-methods.
+# Context Lifecycle
 
-You can also add event listeners which will be executed after the context is
-initialized or before the context is destroyed.
+In order to maintain context scope an application should
+properly initialize and destroy contexts.
 
-Context is initialized when it is first accessed via `Context.get` method.
-You can override default context implementation by setting `cx.context`
-configuration parameter.
+It is done by using `Context.init` and `Context.destroy` methods.
+
+## Events
+
+You can add event listeners which will be executed after the context
+is initialized or before the context is destroyed.
+
+Context is initialized either explicitly or when it is first
+accessed via the `Context.get` method.
 */
 class Context extends HashMap[String, Any] with KeyValueCoercion {
   override def stringPrefix = "ctx"
 }
 
 object Context {
-
-  // We use thread-local storage so that each thread can get it's own instance of context.
 
   protected val threadLocal = new ThreadLocal[Context]
 
@@ -70,7 +73,7 @@ object Context {
   def isLive: Boolean = threadLocal.get != null
 
   def init() {
-    threadLocal.set(cx.instantiate[Context]("cx.context", new Context))
+    threadLocal.set(new Context)
     initListeners.foreach(l => l.apply(get()))
   }
 
@@ -81,8 +84,18 @@ object Context {
     }
   }
 
+  /*! The `executeInNew` method defines the boundaries of the context.
+
+  It wraps the specified `block` inside a context block, which
+  creates and initializes a new instance of Context at the beginning
+  and performs the finalization at the end.
+
+  The most typical scenario of using this method is
+  to perform transaction demarcations in Circumflex ORM.
+  */
   def executeInNew[A](block: Context => A): A = {
-    val previousCtx: Option[Context] = if (isLive) Some(get()) else None
+    val previousCtx: Option[Context] =
+      if (isLive) Some(get()) else None
     try {
       Context.init()
       val _ctx = get()
@@ -97,15 +110,18 @@ object Context {
 
 /*!# Context DSL
 
-Circumflex enables you to use Scala `Symbol` to access and set context variables in a DSL
-fashion.
+Circumflex enables you to use Scala `Symbol` to access
+and set context variables in a DSL fashion.
 
-Following syntaxes are available for setting context variables:
+Following syntaxes is used for setting context variables:
 
-    'key := value                                                               {.scala}
+``` {.scala}
+'key := value
+```
 
-The implicit conversions from `Symbol` into `ContextVarHelper` are available in the
-`pro.savant.circumflex.core` package.
+In order to be able to use such DSL in your application you should
+import the implicit conversion methods from
+the `pro.savant.circumflex.core` package.
 */
 class ContextVarHelper(val sym: Symbol) {
   def :=(value: Any) {
