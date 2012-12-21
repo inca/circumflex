ffbf3cb10385e5be0da6354293c4b2704ed413c1
inca <incarnate@whiteants.net>
2012-12-21 22:35:25 +0400

    Added the auth module.

---
+++ auth/src/main/scala/auth.scala
@@ -0,0 +1,65 @@
+package pro.savant.circumflex
+package auth
+
+import core._, web._
+
+/*! The `Auth` trait maintains basic authentication management logic.
+
+In order to use authentication in your application you should implement
+this trait specifying the `Principal` implementation (e.g. `User`)
+as type parameter.
+*/
+trait Auth[U <: Principal] {
+
+  /*! Principal is resolved from context or from session under key `cx.principal`. */
+  val KEY = "cx.principal"
+
+  /*! The `lookup` method is used to find a `Principal` designated by type parameter
+  `U` by provided unique identifier (see [[auth/src/main/scala/principal.scala]]).
+   */
+  def lookup(principalId: String): Option[U]
+
+  /*! The `anonymous` method should return a constant instance of unauthenticated
+  user. Best practice is to provide a stable transient singleton which overrides
+  all methods of your implementation to deny any state updates.
+  */
+  def anonymous: U
+
+  /*! The `authError` method is used to abort current execution
+  with authentication exception. Default implementation sends
+  HTTP 403 Access Denied, but you can override this method to implement
+  custom authentication error processing logic.
+  */
+  def authError(message: String): Nothing = sendError(403, message)
+
+  /*! The `principalOption` method resolves current principal by first looking up
+  the context and then falling back to session lookup.
+   */
+  def principalOption: Option[U] = ctx.getAs[U](KEY)
+      .orElse(sessionOption.flatMap(
+    _.getString(KEY).flatMap(id => lookup(id))))
+
+  /*! The `principalOrAnonymous`, as the name implies, performs the lookup and
+  returns the anonymous identity as returned by `anonymous` in the case of failure.*/
+  def principalOrAnonymous = principalOption.getOrElse(anonymous)
+
+  /*! The `principal` method returns currently authenticated principal or aborts
+  the execution with `authError`.
+
+  It should only be used in contexts where authentication is mandatory. */
+  def principal = principalOption.getOrElse(
+    authError("No authentication information available."))
+
+}
+
+/*! ## The `NoAuth` stub
+
+If auth is not configured in your application, the `NoAuth` is used
+as main authentication manager with `DummyPrincipal` as principal implementation.
+*/
+object NoAuth extends Auth[DummyPrincipal] {
+
+  def anonymous = DummyPrincipal
+
+  def lookup(principalId: String) = None
+}
---
+++ auth/src/main/scala/principal.scala
@@ -0,0 +1,72 @@
+package pro.savant.circumflex
+package auth
+
+import core._
+
+/*! # Principal
+
+The `Principal` trait should be implemented in your application
+by the `User` class (or whatever abstraction you use for authentication).
+
+Typically you would use an ORM library to provide methods for storing and
+retrieving users, but your application may choose different technology
+for managing authentication data. The `Principal` trait in this sense is
+agnosting to storage technology. However, the authentication architecture
+provided by Circumflex
+*/
+trait Principal {
+
+  /*! A unique ID is used to lookup the user from the storage and to associate
+  it with session on current domain. The `uniqueId` method should return string
+  representation (for use in cookies). The same string is to be used in the
+  implementation of method `lookup` in `Auth`.
+   */
+  def uniqueId: String
+
+  /*! The `checkSession`, `registerSession` and `purgeSessions` methods
+  are used in SSO to ensure that the logout occurs on all domains bound
+  to the application. This implies that session ids are stored along with `Principal`
+  (probably in a separate table).
+
+  The `locationId` parameter is a random string generated at `authBase` which
+  is used to logically group sessions on different domains opened from
+  the same user agent (which could mean the same machine, same location, etc.).
+
+  The `checkSession` should return `true` if the current session is associated
+   with the principal at specified `locationId`.
+
+   The `registerSession` should associate the id of current session with specified
+  `locationId` for the principal.
+
+  The `purgeSession` removes all associations of session ids with specified `locationId`
+  for the principal.
+  */
+
+  def checkSession(locationId: String): Boolean
+
+  def registerSession(locationId: String)
+
+  def purgeSessions(locationId: String)
+
+}
+
+/*! ## Stubs
+
+Circumflex Auth includes default implementations of `Principal` which
+are used by `NoAuth` stub.
+*/
+
+class DummyPrincipal extends Principal {
+
+  def uniqueId = "dummy"
+
+  def checkSession(locationId: String) = false
+
+  def registerSession(locationId: String) {}
+
+  def purgeSessions(locationId: String) {}
+
+}
+
+object DummyPrincipal extends DummyPrincipal
+
