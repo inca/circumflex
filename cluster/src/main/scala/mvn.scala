fb1dd354c6ef6b2f0ed304388f364651aaad9cec
inca <incarnate@whiteants.net>
2013-04-03 12:06:43 +0400

    Added subprojects reading.

--- cluster/src/main/scala/mvn.scala
+++ cluster/src/main/scala/mvn.scala
@@ -4,13 +4,65 @@ package cluster
 import xml._
 import java.io.File
 
+trait Artifact extends StructHolder {
+
+  val _groupId = text("groupId")
+  def groupId = _groupId.getOrElse("")
+
+  val _artifactId = text("artifactId")
+  def artifactId = _artifactId.getOrElse("")
+
+  val _version = text("version")
+  def version = _version.getOrElse("")
+
+  val _packaging = text("packaging")
+  def packaging = _packaging.getOrElse("jar")
+
+}
+
+class Dependency extends Artifact {
+
+  def elemName = "dependency"
+
+}
+
+class ProjectProperty(val name: String)
+    extends TextHolder {
+
+  def elemName = name
+
+}
+
 class Project(val baseDir: File)
     extends StructHolder
-    with XmlFile { project =>
+    with XmlFile
+    with Artifact { project =>
 
   def descriptorFile = new File(baseDir, "pom.xml")
 
   def elemName = "project"
+
+  val _name = text("name")
+  val _description = text("description")
+  val _url = text("url")
+
+  val _parent = new Artifact {
+    def elemName = "parent"
+  }
+
+  val dependencies = new ListHolder[Artifact] {
+
+    def elemName = "dependencies"
+
+    def read = {
+      case "dependency" => new Dependency
+    }
+
+  }
+
+  val dependencyManagement = new StructHolder {
+    def elemName = "dependencyManagement"
+  }
 
   val modules = new ListHolder[Module] {
 
@@ -20,7 +72,46 @@ class Project(val baseDir: File)
       case "module" => new Module(project)
     }
 
+    def projects = children.flatMap(_.project).map(_.load())
+
   }
+
+  val properties = new ListHolder[ProjectProperty] {
+
+    def elemName = "properties"
+
+    def read = {
+      case n => new ProjectProperty(n)
+    }
+
+  }
+
+  val build = new StructHolder {
+
+    def elemName = "build"
+
+  }
+
+  val reporting = new StructHolder {
+
+    def elemName = "reporting"
+
+  }
+
+  val profiles = new StructHolder {
+
+    def elemName = "profiles"
+
+  }
+
+  def subprojects: Seq[Project] =
+    modules.projects.flatMap(p => Seq(p) ++ p.subprojects)
+
+  def getArtifactPath = groupId.replaceAll("\\.", "/") + "/" +
+      version + "/" +
+      artifactId + "-" + version + "." + packaging
+
+  override def toString = groupId + ":" + artifactId + ":" + version
 
 }
 
